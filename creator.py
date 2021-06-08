a45021c2330055d8b4b6a130ff28490d01082944
Spetz <73785089+Pspetz@users.noreply.github.com>
2021-06-08 14:24:16 +0300

    Update creator.py

--- creator.py
+++ creator.py
@@ -2,7 +2,7 @@ import mysql.connector as mariadb
 
 mariadb_connection=mariadb.connect(user="root",password="",host="Localhost",port="3306",database="Tourism_Statistics")
 create_cursor = mariadb_connection.cursor()
-create_cursor.execute("CREATE TABLE  IF NOT EXISTS Greece_Stats(year INT NOT NULL PRIMARY KEY,tourists INT)")
+create_cursor.execute("CREATE TABLE  IF NOT EXISTS statistika(year INT NOT NULL PRIMARY KEY,tourists INT)")
 
 for x in create_cursor:
     print(x)
