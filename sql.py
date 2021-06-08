81621b91b32ef3ed576e7a53912cd0b429da867f
Spetz <73785089+Pspetz@users.noreply.github.com>
2021-06-08 14:26:03 +0300

    Update sql.py

--- sql.py
+++ sql.py
@@ -5,19 +5,19 @@ import DBManager
 import csv
 import pandas as pd
 
-mariadb_connection=mariadb.connect(user="root",password="",host="Localhost",port="3306",database="Tourism_Statistics")
+mariadb_connection=mariadb.connect(user="root",password="",host="Localhost",port="3306",database="Greece_Stats")
 
 create_cursor = mariadb_connection.cursor()
 def synolikes_afikseis_touristwn (year,row,col,total_arrivals,sheet):
 
-    #h sthlh me tis synolikes afikseis ths swsths xronias einai 2 sthles dipla apo thn sthlh pou periexei to geniko synolo
+ 
     tourists = round(sheet.cell_value(row, col + 2))
-    sql = "INSERT IGNORE INTO Greece_Stats (year,total_tourists) VALUES(%s,%s)"
+    sql = "INSERT INTO statistika (year,total_tourists) VALUES(%s,%s)"
     val = (year,tourists)
     cursor.execute(sql,val)
     db.mydb.commit()
-    print("Sucessful Parse of Data in table Greece_Stats")
-    csv_output_file(['ΕΤΟΣ','ΣΥΝΟΛΙΚΟΙ ΤΟΥΡΙΣΤΕΣ'],"ΣΥΝΟΛΙΚΕΣ ΑΦΙΞΕΙΣ ΤΟΥΡΙΣΤΩΝ ΓΙΑ ΤΟ 2011-2013.csv","tourism_per_year")
+    print("Epituximeno perasma statistikos sthn vasi Greece_Stats")
+    csv_output_file(['XRONOLOGIA','TOURISTES'],"ΣΥΝΟΛΙΚΕΣ ΑΦΙΞΕΙΣ ΤΟΥΡΙΣΤΩΝ ΓΙΑ ΤΟ 2015-2019.csv","tourism_per_year")
 
  
 
