1081efee8feb93443ac3c814a8e7042ff57dc9d8
Spetz <73785089+Pspetz@users.noreply.github.com>
2021-06-24 05:18:29 +0300

    Delete creator.py

--- creator.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector as mariadb
-
-mariadb_connection=mariadb.connect(user="root",password="",host="Localhost",port="3306",database="Tourism_Statistics")
-create_cursor = mariadb_connection.cursor()
-create_cursor.execute("CREATE TABLE  IF NOT EXISTS statistika(year INT NOT NULL PRIMARY KEY,tourists INT)")
-
-for x in create_cursor:
-    print(x)
